23307f56f0182b1fc929c57524002e8e3c721f92
saner57 <sandrinemane@gmail.com>
2018-08-14 15:17:09 +0000

    first commit

---
+++ test2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Mediatechdynamique</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">>
+</head>
+
+<body>
+	<?php  
+
+
+	?>
+
+</body>
+</html>
